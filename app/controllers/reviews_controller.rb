3e97b471f278b438addddeaadd622cd4488e91fe
Xia Dong <dongxiapp@gmail.com>
2019-04-19 22:05:59 +0000

    allow users to create reviews and display them on the product show page

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to @product
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
