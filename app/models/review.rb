fbee1b96203677190d9e7bfd42de01acd64a3c38
Xia Dong <dongxiapp@gmail.com>
2019-04-19 18:28:10 +0000

    add validations to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+
 end
--- db/migrate/20190419181424_create_reviews.rb
+++ db/migrate/20190419181424_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.reference :product
-      t.reference :user
+      t.references :product
+      t.references :user
       t.text :description
       t.integer :rating
 
