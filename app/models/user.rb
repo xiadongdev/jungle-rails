31e999a7b5d4608d6405c43f7b74fc7d7a5fe8e8
Xia Dong <dongxiapp@gmail.com>
2019-04-22 19:46:33 +0000

    add tests on User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, :uniqueness => { :case_sensitive => false }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before :each do
+    @user = User.new(
+      name: 'Ruby',
+      email: 'r@r.com',
+      password: 'r',
+      password_confirmation: 'r'
+    )
+  end
+
+  describe 'Validation' do
+    it 'is valid if password and password_confirmation are the same' do
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid if password and password_confirmation are not the same' do
+      @user.password_confirmation = 'k'
+      expect(@user).to_not be_valid
+    end
+    
+    it 'is not valid if password and password_confirmation are not provided' do
+      @user.password_confirmation = nil
+      expect(@user).to_not be_valid
+    end
+    
+    it 'is not valid if name is not provided' do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+    
+    it 'is not valid if email is not provided' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+    
+    it 'is not valid if email already exists in database, not case sensitive' do
+      @user.save
+      another_user = User.create(name: 'Sam', email: 'R@r.cOM', password: 's', password_confirmation: 's')
+      expect(another_user).to_not be_valid
+    end
+    
+  end
+end
